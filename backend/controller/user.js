fb125a6f4c5b8f8fd5a99e2f10b5e5b03ab446cf
Madhav Girdhar <mgirdhar027@gmail.com>
2024-01-31 03:30:35 +0530

    loginUser error Resolved

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,8 +1,10 @@
 const { validationResult } = require("express-validator");
+require('dotenv').config()
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const model = require('../model/user');
 const User = model.User;
+const JWT_SECRET = "HelloIamagoodBoy";
 
 exports.getAlluser = async (req,res)=>{
     const user = await User.find();
@@ -38,10 +40,40 @@ exports.createUser = async (req,res)=>{
             }
         }
         const authtoken = jwt.sign(data,process.env.JWT_SECRET);
-
         res.json({authtoken});
     }catch(error){
         console.error(error.message);
         res.status(500).send("Some error occured");
+    }
+}
+
+exports.loginUser = async (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({error: errors.array() });
+    }
+
+    // const [email,password] = req.body;
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            res.status(400).json({error:"Please try to login using correct credentials"});
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            res.status(400).json({error:"Please try to login using correct credentials"});
+        }
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data,process.env.JWT_SECRET);
+        res.json({authtoken});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server error");
     }
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router
     .get('/',userController.getAlluser)
-    .post('/createUser',[body('email','Enter a valid Email id').isEmail(),body('name','Name length should be greater than 3').isLength({min:3}),body('password','Password length must be greater than 5').isLength({min:5})],userController.createUser);
+    .post('/createUser',[body('email','Enter a valid Email id').isEmail(),body('name','Name length should be greater than 3').isLength({min:3}),body('password','Password length must be greater than 5').isLength({min:5})],userController.createUser)
+    .post('/login',[body('email','Enter a valid Email id').isEmail(),body('password','Password can not be blank').exists()],userController.loginUser);
 
 exports.router = router;
